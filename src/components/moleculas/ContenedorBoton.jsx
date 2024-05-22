820f664227b82da5c1996025fdb4be72230716e8
RamiroQuiro <rama.exe.13@gmail.com>
2024-05-22 15:49:44 -0300

    implementando button con modal

---
+++ src/components/moleculas/ContenedorBoton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ContenedorBoton({children}) {
+
+
+const functionClick=()=>{
+    
+}
+
+    return (
+        <div class="w-full flex items-center gap-x-2 justify-end my-3">
+            {children}
+        </div>
+    )
+}
+
