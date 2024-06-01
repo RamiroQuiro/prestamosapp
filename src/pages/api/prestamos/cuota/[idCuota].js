980f7995a708aacec98f1386197c3b42c3a4dbf3
ramiroquiro <rama.exe.13@gmail.com>
2024-06-01 01:57:01 -0300

    tratando de cerrar los prestamos cuando todas cuotas esten pagas

--- src/pages/api/prestamos/cuota/[idCuota].js
+++ src/pages/api/prestamos/cuota/[idCuota].js
@@ -1,8 +1,7 @@
-import { Cuota, db, eq } from "astro:db";
+import { Cuota, Prestamo, db, eq } from "astro:db";
 
 export async function PUT({ params }) {
     const { idCuota } = params;
-    console.log(idCuota)
     try {
         const cuotaFind = await db.update(Cuota)
             .set({ pagada: true }).where(eq(Cuota.id, idCuota))
@@ -14,6 +13,18 @@ export async function PUT({ params }) {
         }
 
 
+        // queriendo cerrar el prestamo
+//         // Obtén el id del préstamo asociado a la cuota
+//         const cuotaFindex = await db.select().from(Cuota).where(eq(Cuota.id,idCuota))
+//         const idPrestamo=cuotaFindex[0].prestamoId
+//         console.log(idPrestamo)
+//         // Cuenta cuántas cuotas no están pagadas para este préstamo
+//         const cuotasNoPagadas = await db.select().from(Prestamo).where(eq(Prestamo.id, idPrestamo), eq(Cuota.pagada, false)).count();
+// console.log('cuotas no pagadas',cuotasNoPagadas)
+//         // Si no hay cuotas no pagadas, actualiza el estado del préstamo a 'cerrado'
+//         if (cuotasNoPagadas == 0) {
+//             await db.update(Prestamo).set({ estado: 'cerrado' }).where(eq(Prestamo.id, idPrestamo));
+//         }
         return new Response(JSON.stringify({
             status: 200,
             data: 'cuota cancelada'
