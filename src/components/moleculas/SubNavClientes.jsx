155141729f6489cf4ac7afcd5805adea95805cc5
RamiroQuiro <rama.exe.13@gmail.com>
2024-06-28 12:26:36 -0300

    corrigiendo submenues

--- src/components/moleculas/SubNavClientes.jsx
+++ src/components/moleculas/SubNavClientes.jsx
@@ -1,37 +1,53 @@
 import React from 'react'
 import LinkSubMenu from '../atomos/LinkSubMenu'
 
-export default function SubNavClientes({ idClients ,dataCliente}) {
+export default function SubNavClientes({ idClients, pathname, dataCliente }) {
+
+  const links = [
+    {
+      href: `/dashboard/clientes`,
+      children: 'clientes'
+    },
+    {
+      href: `/dashboard/clientes/${idClients}`,
+      children: `  ${dataCliente?.nombre + ' ' + dataCliente?.apellido}`,
+    },
+    {
+      href: `/dashboard/clientes/${idClients}/datosPersonales`,
+      children: 'Datos personales'
+    },
+    {
+      href: `/dashboard/clientes/${idClients}/creditos`,
+      children: 'creditos'
+    },
+    {
+      href: `/dashboard/clientes/${idClients}/pagos`,
+      children: 'pagos'
+    }
+  ]
+
   return (
     <div className='flex items-center justify-start gap-4  w-full px-5'>
       <LinkSubMenu
-        href={`/dashboard/clientes`}
+        href={links[0].href}
+        isActive={links[0].href.toLowerCase().includes(pathname.toLowerCase())}
       >
-        Clientes
+        {links[0].children}
       </LinkSubMenu>
-      {idClients && <>
-        <LinkSubMenu
-          href={`/dashboard/clientes/${idClients}`}
-        >
-          {dataCliente.nombre+' '+dataCliente.apellido}
-        </LinkSubMenu>
-        <LinkSubMenu
-          href={`/dashboard/clientes/${idClients}/datosPersonales`}
-        >
-          Datos Cliente
-        </LinkSubMenu>
-        <LinkSubMenu
-          href={`/dashboard/clientes/${idClients}/creditos`}
-        >
-          Creditos
-        </LinkSubMenu>
-        <LinkSubMenu
-          href={`/dashboard/clientes/${idClients}/pagos`}
-        >
-        Pagos
-        </LinkSubMenu>
-      </>
+      {idClients &&
+        links?.slice(1,-1)?.map((elemtne, i) => {
 
+          return (
+            <LinkSubMenu
+            key={i}
+              href={elemtne.href}
+              isActive={elemtne.href.toLowerCase()==pathname.toLowerCase()}
+            >
+              {elemtne.children}
+            </LinkSubMenu>
+          )
+        }
+        )
 
       }
 
