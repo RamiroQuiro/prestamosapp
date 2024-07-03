f16144ae6307c9da714b3e8239217e471698f8ce
RamiroQuiro <rama.exe.13@gmail.com>
2024-07-03 16:14:57 -0300

    pintando totales en el formulario

--- src/lib/formulas/calculoMora.js
+++ src/lib/formulas/calculoMora.js
@@ -1,15 +1,15 @@
 function calcularMora(principal, tasaMora, diasRetraso) {
-    const tasaMoraDiaria = tasaMora / 365;
+    const tasaMoraDiaria = Number(tasaMora) / 365;
     const mora = principal * tasaMoraDiaria * diasRetraso;
+    
     return mora;
   }
   
-  // Ejemplo de uso
-  const principal = 1000;
-  const tasaMora = 0.15; // 15% anual
-  const diasRetraso = 30;
+//   // Ejemplo de uso
+//   const principal = 86000;
+//   const tasaMora = 0.6246; // 15% anual
+//   const diasRetraso = 2;
   
-  const moraCalculada = calcularMora(principal, tasaMora, diasRetraso);
-  console.log(`La mora es: $${moraCalculada.toFixed(2)}`);
-
+//   const moraCalculada = calcularMora(principal, tasaMora, diasRetraso);
+// console.log(moraCalculada)
 export {calcularMora}
