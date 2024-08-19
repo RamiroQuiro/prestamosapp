c451d0cedf8e01be7d50ccfd452f523e3e2ddcfe
RamiroQuiro <rama.exe.13@gmail.com>
2024-08-19 10:58:03 -0300

    notificaciones al cargar un cliente repetido el dni

--- src/pages/api/clientes/route.js
+++ src/pages/api/clientes/route.js
@@ -6,76 +6,77 @@ export const generateUidEmail = (email) => {
 };
 
 export const generateUid = () => {
-  return randomBytes(16).toString('hex');
+  return randomBytes(16).toString("hex");
 };
 export async function GET({ params }) {
-try {
-  
-  return new Response(
-    JSON.stringify({
-      data: "hello andando men",
-    })
-  );
-} catch (error) {
-  
-  return new Response(
-    JSON.stringify({
-      data: "error",
-    },{
-      code:404
-    })
-  );
-}
+  try {
+    return new Response(
+      JSON.stringify({
+        data: "hello andando men",
+      })
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify(
+        {
+          data: "error",
+        },
+        {
+          code: 404,
+        }
+      )
+    );
+  }
 }
 
 export async function POST({ request }) {
-  const { email, nombre, apellido, dni, direccion, cel ,userId} = await request.json();
-// console.log('email:',email, ' nombre',nombre, ' apellido',apellido,' dni',dni, ' direccion',direccion,' cel',cel,' userId',userId )
-try {
-   
-    const clientExist = await db.select().from(Cliente).where(eq(Cliente.usuarioId, userId))
+  const { email, nombre, apellido, dni, direccion, cel, userId } =
+    await request.json();
+  // console.log('email:',email, ' nombre',nombre, ' apellido',apellido,' dni',dni, ' direccion',direccion,' cel',cel,' userId',userId )
+  try {
+    const clientExist = await db
+      .select()
+      .from(Cliente)
+      .where(eq(Cliente.dni,dni))
 
-    
-    if (clientExist.includes(element=>element.email==email)) {
-      console.log(`cliente ya registrado con el email ${email}.`);
+    if (clientExist[0]) {
       return new Response(
         JSON.stringify({
           status: 400,
-        }))
+          msg: `cliente ya registrado con el dni ${dni}.`,
+        })
+      );
     }
 
 
- const id = generateUidEmail(email);
+    const id = generateUidEmail(email);
     // const now = new Date()// Crea un nuevo objeto Date y conviértelo a una cadena ISO
 
     const createUser = await db.insert(Cliente).values({
       id: id,
-      usuarioId:userId,
-      email:email,
-      nombre:nombre,
-      apellido:apellido,
-      direccion:direccion,
+      usuarioId: userId,
+      email: email,
+      nombre: nombre,
+      apellido: apellido,
+      direccion: direccion,
       celular: cel,
       dni: dni,
       // fechaActualizacion:now,
-      
     });
 
-
     return new Response(
-        JSON.stringify({
-          data: "andando",
-          status: 200,
-        }))
+      JSON.stringify({
+        data: "andando",
+        status: 200,
+      })
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return new Response(
-        JSON.stringify({
-          data: "error, no andando",
-          status: 400,
-        }))
-
+      JSON.stringify({
+        data: "error, no andando",
+        status: 400,
+      })
+    );
   }
-  
-  
 }
