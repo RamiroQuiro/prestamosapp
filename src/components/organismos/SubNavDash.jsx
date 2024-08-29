157186a9642ef6a70cb481519d4714615b7d2a4f
ramiroquiro <rama.exe.13@gmail.com>
2024-08-28 23:50:09 -0300

    cargando dashboard subnav

--- src/components/organismos/SubNavDash.jsx
+++ src/components/organismos/SubNavDash.jsx
@@ -3,24 +3,24 @@ import SubNavPrestamo from '../moleculas/SubNavPrestamo'
 import SubNavClientes from '../moleculas/SubNavClientes'
 import SubNavAjustes from '../moleculas/SubNavAjustes'
 
-export default function SubNavDash({ pathname,idPrestamo,idCliente,dataCliente }) {
+export default function SubNavDash({ pathname, idPrestamo, idCliente, dataCliente }) {
 
     if (pathname.startsWith('/dashboard/prestamos')) {
         return (
-            <SubNavPrestamo  pathname={pathname}  idPrestamo={idPrestamo}/>
+            <SubNavPrestamo pathname={pathname} idPrestamo={idPrestamo} />
         )
     } else if (pathname.includes('clientes')) {
         return (
-         <SubNavClientes pathname={pathname} idClients={idCliente} dataCliente={dataCliente}/>
+            <SubNavClientes pathname={pathname} idClients={idCliente} dataCliente={dataCliente} />
         )
     } else if (pathname.includes('ajustes')) {
         return (
-         <SubNavAjustes pathname={pathname} idClients={idCliente} dataCliente={dataCliente}/>
+            <SubNavAjustes pathname={pathname} idClients={idCliente} dataCliente={dataCliente} />
         )
     } else if (pathname.startsWith('/dashboard')) {
         return (
             <div className='flex items-center justify-start gap-4  w-full px-5'>
-ramiro.quiroga
+                <div className='text-xs font-thin capitalize rounded-lg px-2 py-1 border border-primary-200/50 hover:border-primary-100 duration-200 hover:bg-primary-200/10'>Dashboard 👇🏼</div>
 
             </div>
         )
