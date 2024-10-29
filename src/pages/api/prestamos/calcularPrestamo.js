fae68895975a6ef25ba98ccb27a260076f1720a1
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2024-10-29 10:38:16 -0300

    Guardando cambios locales antes de revertir

--- src/pages/api/prestamos/calcularPrestamo.js
+++ src/pages/api/prestamos/calcularPrestamo.js
@@ -3,19 +3,25 @@ import { evaluate } from "mathjs";
 
 export async function POST({ request }) {
     const { usuarioId, importe, tasaInteres, nCuotas } = await request.json();
-console.log(usuarioId)
+console.log(usuarioId,importe,tasaInteres,nCuotas)
     try {
         // Obtén el usuario para verificar si tiene una fórmula personalizada
         const { formulaPersonalizada,tasaInteress } = (await db.select({ formulaPersonalizada: User.formulaPersonalizada,tasaInteress:Intereses.value }).from(User).innerJoin(Intereses,and(eq(Intereses.usuarioId,usuarioId),eq(Intereses.selectDefault,true))).where(eq(User.id, usuarioId))).at(0)
         // Definir fórmula por defecto usando el sistema de amortización francés si no hay fórmula personalizada
         let formula = formulaPersonalizada || "(capital * ((tasaInteres / 100 / 12) * (1 + tasaInteres / 100 / 12) ^ cuotas)) / ((1 + tasaInteres / 100 / 12) ^ cuotas - 1)";
 
+        
         // Preparar variables comunes para evaluación
-        const tasaInteresDecimal = tasaInteres ||tasaInteress // para convertir tasa de interés a decimal mensual agregar /100 /12
-        let montoTotal = 0;
+        const tasaInteresDecimal = tasaInteres || tasaInteress // para convertir tasa de interés a decimal mensual agregar /100 /12
+        let montoTotal = 0; 
         let cuotasArray = [];
         let saldoPendiente = importe; // Inicializar saldo pendiente con el capital inicial
 
+        if (!tasaInteresDecimal) {
+            return new Response(JSON.stringify({
+                message:'falta configuracion interes'
+            }), { status: 500 })
+        }
         // Calcular cada cuota de forma individual y acumular el monto total
         for (let i = 1; i <= nCuotas; i++) {
             const variables = {
